b3a108b456b4c4d3e88a27457d9010477aa23482
Andrew <creativedrewy@gmail.com>
2015-07-21 14:45:05 -0500

    Start writing some more markup parsing code.

--- src/main/scala/BeerAdvocateInfo.scala
+++ src/main/scala/BeerAdvocateInfo.scala
@@ -6,20 +6,22 @@ import net.ruippeixotog.scalascraper.scraper.ContentExtractors._
  *
  */
 class BeerAdvocateInfo {
+  private var baseUrl = "http://www.beeradvocate.com"
 
   def getBeerInfo(searchTerm: String): Unit = {
     val searchTerm = "Summit+Extra+Pale+Ale"
 
     val browser = new Browser
-    val searchPageContents = browser.get("http://www.beeradvocate.com/search/?q=" + searchTerm + "&qt=beer")
+    val searchPageContents = browser.get(baseUrl + "/search/?q=" + searchTerm + "&qt=beer")
 
     val initSearchResults = searchPageContents >> extractor("#ba-content > div", elementList) >> extractor("a", elementList) filter { list => list.nonEmpty }
 
     if (initSearchResults.nonEmpty && initSearchResults.head.length > 1) {
       val beerInfoElement = initSearchResults.head.head
-
       val beerUrl = beerInfoElement.attr("href")
 
+      var beerPageContents = browser.get(baseUrl + beerUrl)
+      beerPageContents >> extractor("a", elementList) filter { el => el.attr("href").contains("/beer/style") } foreach println
     } else
       println("Sorry I couldn't find any results for that beer. Try to be more specific.")
   }
